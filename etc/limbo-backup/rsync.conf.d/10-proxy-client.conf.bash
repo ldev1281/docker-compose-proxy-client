75fde162f4e2d8adc3ccb106afa34f778b36d1b9
Andrew Kornilov <akornilov@gmail.com>
2025-08-14 02:43:28 +0300

    Set backup and restore commands the same

--- etc/limbo-backup/rsync.conf.d/10-proxy-client.conf.bash
+++ etc/limbo-backup/rsync.conf.d/10-proxy-client.conf.bash
@@ -1,7 +1,7 @@
 CMD_BEFORE_BACKUP="docker compose --project-directory /docker/proxy-client down"
 CMD_AFTER_BACKUP="docker compose --project-directory /docker/proxy-client up -d"
-CMD_BEFORE_RESTORE="docker compose --project-directory /docker/proxy-client stop"
-CMD_AFTER_RESTORE="docker compose --project-directory /docker/proxy-client start"
+CMD_BEFORE_RESTORE="docker compose --project-directory /docker/proxy-client down"
+CMD_AFTER_RESTORE="docker compose --project-directory /docker/proxy-client up -d"
 
 INCLUDE_PATHS=(
   "/docker/proxy-client/.env"
