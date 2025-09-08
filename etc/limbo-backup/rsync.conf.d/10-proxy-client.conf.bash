071cfcf31620ed6667f64f3142f21129fc1acd32
root <kireev1820@gmail.com>
2025-09-08 17:15:36 +0000

    Added after restore param

--- etc/limbo-backup/rsync.conf.d/10-proxy-client.conf.bash
+++ etc/limbo-backup/rsync.conf.d/10-proxy-client.conf.bash
@@ -1,6 +1,8 @@
 CMD_BEFORE_BACKUP="docker compose --project-directory /docker/proxy-client down"
 CMD_AFTER_BACKUP="docker compose --project-directory /docker/proxy-client up -d"
 
+CMD_AFTER_RESTORE="docker compose --project-directory /docker/proxy-client restart"
+
 INCLUDE_PATHS=(
   "/docker/proxy-client"
 )
