240207d8a47a591332e9788689fc2f01001634fd
root <kireev1820@gmail.com>
2025-09-12 12:11:57 +0400

    Update 10-proxy-client.conf.bash
    add to cmd after restore network create command

--- etc/limbo-backup/rsync.conf.d/10-proxy-client.conf.bash
+++ etc/limbo-backup/rsync.conf.d/10-proxy-client.conf.bash
@@ -1,7 +1,10 @@
 CMD_BEFORE_BACKUP="docker compose --project-directory /docker/proxy-client down"
 CMD_AFTER_BACKUP="docker compose --project-directory /docker/proxy-client up -d"
 
-CMD_AFTER_RESTORE="docker compose --project-directory /docker/proxy-client restart"
+CMD_AFTER_RESTORE=(
+"docker network create --driver bridge proxy-client-authentik || true"
+"docker compose --project-directory /docker/proxy-client restart"
+)
 
 INCLUDE_PATHS=(
   "/docker/proxy-client"
