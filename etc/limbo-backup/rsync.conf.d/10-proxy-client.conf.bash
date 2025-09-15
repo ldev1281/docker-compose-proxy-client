6750f98b196391d745eda2778e77da71bb1de8f0
root <kireev1820@gmail.com>
2025-09-15 20:15:03 +0400

    Update 10-proxy-client.conf.bash
    bug fix
    
    restart to up -d
    add down before restore

--- etc/limbo-backup/rsync.conf.d/10-proxy-client.conf.bash
+++ etc/limbo-backup/rsync.conf.d/10-proxy-client.conf.bash
@@ -1,9 +1,10 @@
 CMD_BEFORE_BACKUP="docker compose --project-directory /docker/proxy-client down"
 CMD_AFTER_BACKUP="docker compose --project-directory /docker/proxy-client up -d"
 
+CMD_BEFORE_RESTORE="docker compose --project-directory /docker/proxy-client down || true"
 CMD_AFTER_RESTORE=(
 "docker network create --driver bridge proxy-client-authentik || true"
-"docker compose --project-directory /docker/proxy-client restart"
+"docker compose --project-directory /docker/proxy-client up -d"
 )
 
 INCLUDE_PATHS=(
